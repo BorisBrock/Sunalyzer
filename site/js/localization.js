523cc2453d06592bb7a42a6542bda0ff02d5ad74
VanKurt <gabrielborisbrock@gmail.com>
2022-08-23 09:58:52 +0200

    Improved chart string

--- site/js/localization.js
+++ site/js/localization.js
@@ -88,7 +88,7 @@ let chartStrings = [
     ["chart_self_consumed", "Self consumed", "Eigenverbrauch"],
     ["chart_produced_self_kwh", "Consumed directly [kWh]", "Direktverbrauch [kWh]"],
     ["chart_produced_grid_kwh", "Feed-in [kWh]", "Einspeisung [kWh]"],
-    ["chart_consumed_pv_kwh", "Consumed directly [kWh]", "Direktverbrauch [kWh]"],
+    ["chart_consumed_pv_kwh", "From PV [kWh]", "Aus PV [kWh]"],
     ["chart_consumed_grid_kwh", "From grid [kWh]", "Netzbezug [kWh]"],
 ];
 
