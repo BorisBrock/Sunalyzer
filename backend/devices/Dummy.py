83da5d6b9f18f3bea05d65e035ff5347dcb1571f
VanKurt <gabrielborisbrock@gmail.com>
2022-08-25 05:08:31 +0200

    Update Dummy.py

--- backend/devices/Dummy.py
+++ backend/devices/Dummy.py
@@ -24,8 +24,8 @@ class Dummy:
         self.total_energy_consumed_kwh = self.total_energy_consumed_kwh + 1
         self.total_energy_fed_in_kwh = self.total_energy_fed_in_kwh + 1
 
-        #self.current_power_produced_kw = self.current_power_produced_kw
-        #self.current_power_consumed_from_grid_kw = self.current_power_consumed_from_grid_kw
-        #self.current_power_consumed_from_pv_kw = self.current_power_consumed_from_pv_kw
-        #self.current_power_consumed_total_kw = self.current_power_consumed_total_kw
-        #self.current_power_fed_in_kw = self.current_power_fed_in_kw
+        # self.current_power_produced_kw = self.current_power_produced_kw
+        # self.current_power_consumed_from_grid_kw = self.current_power_consumed_from_grid_kw
+        # self.current_power_consumed_from_pv_kw = self.current_power_consumed_from_pv_kw
+        # self.current_power_consumed_total_kw = self.current_power_consumed_total_kw
+        # self.current_power_fed_in_kw = self.current_power_fed_in_kw
