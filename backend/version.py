a8442798ebe0de7e36ea84af32dc7e54015fe122
VanKurt <gabrielborisbrock@gmail.com>
2022-08-25 15:05:45 +0200

    Updated version to 0.10.0

--- backend/version.py
+++ backend/version.py
@@ -1,4 +1,4 @@
-Version = "0.9.0"
+Version = "0.10.0"
 
 
 def get_version():
--- site/js/version.js
+++ site/js/version.js
@@ -1,5 +1,5 @@
 
-const Version = "0.9.0";
+const Version = "0.10.0";
 
 function setVersion() {
     document.getElementById("version").innerHTML = "Version " + Version;
