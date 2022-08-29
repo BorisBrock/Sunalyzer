e9460850d23e08c80d5fd2f4b36add4e85b12c9d
VanKurt <gabrielborisbrock@gmail.com>
2022-08-29 15:13:00 +0200

    Version bump to 0.14.0

--- backend/version.py
+++ backend/version.py
@@ -1,4 +1,4 @@
-Version = "0.13.0"
+Version = "0.14.0"
 
 
 def get_version():
--- site/js/version.js
+++ site/js/version.js
@@ -1,5 +1,5 @@
 
-const Version = "0.13.0";
+const Version = "0.14.0";
 
 function setVersion() {
     document.getElementById("version").innerHTML = "Version " + Version;
