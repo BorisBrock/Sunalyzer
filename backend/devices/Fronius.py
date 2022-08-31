ff59fc74dab1e7be07d3e4a2bcc79bc886cfca68
VanKurt <gabrielborisbrock@gmail.com>
2022-08-31 11:34:35 +0200

    Bugfixes

--- backend/devices/Fronius.py
+++ backend/devices/Fronius.py
@@ -35,39 +35,39 @@ class Fronius:
     def copy_data(self, inverter_data, meter_data):
         '''Copies the results from the API request.'''
         # Inverter data
-        str_total_produced = inverter_data["Body"]["Data"]["Site"]["E_Total"]
-        total_produced = float(str_total_produced)
+        str_total_produced_wh = inverter_data["Body"]["Data"]["Site"]["E_Total"]
+        total_produced_kwh = float(str_total_produced_wh) * 0.001
         # Meter data
-        str_total_consumed_from_grid = meter_data["Body"]["Data"]["0"]["EnergyReal_WAC_Plus_Absolute"]
-        total_consumed_from_grid = float(str_total_consumed_from_grid)
-        str_total_fed_in = meter_data["Body"]["Data"]["0"]["EnergyReal_WAC_Minus_Absolute"]
-        total_fed_in = float(str_total_fed_in)
+        str_total_consumed_from_grid_wh = meter_data["Body"]["Data"]["0"]["EnergyReal_WAC_Plus_Absolute"]
+        total_consumed_from_grid_kwh = float(str_total_consumed_from_grid_wh) * 0.001
+        str_total_fed_in_wh = meter_data["Body"]["Data"]["0"]["EnergyReal_WAC_Minus_Absolute"]
+        total_fed_in_kwh = float(str_total_fed_in_wh) * 0.001
         # Compute other values
-        total_self_consumption = total_produced - total_fed_in
-        total_consumption = total_consumed_from_grid + total_self_consumption
+        total_self_consumption_kwh = total_produced_kwh - total_fed_in_kwh
+        total_consumption_kwh = total_consumed_from_grid_kwh + total_self_consumption_kwh
 
         # Logging
         if self.verbose_logging:
             print(f"Fronius device: Absolute values:\n"
-                  f" - Total produced: {str(total_produced)}\n"
-                  f" - Total grid consumption: {str(total_consumed_from_grid)}\n"
-                  f" - Total self consumption: {str(total_self_consumption)}\n"
-                  f" - Total consumption: {str(total_consumption)}\n"
-                  f" - Total fed in: {str(total_fed_in)}")
+                  f" - Total produced: {str(total_produced_kwh)} kWh\n"
+                  f" - Total grid consumption: {str(total_consumed_from_grid_kwh)} kWh\n"
+                  f" - Total self consumption: {str(total_self_consumption_kwh)} kWh\n"
+                  f" - Total consumption: {str(total_consumption_kwh)} kWh\n"
+                  f" - Total fed in: {str(total_fed_in_kwh)} kWh")
 
         # Total/absolute values
-        self.total_energy_produced_kwh = total_produced
-        self.total_energy_consumed_kwh = total_consumption
-        self.total_energy_fed_in_kwh = total_fed_in
+        self.total_energy_produced_kwh = total_produced_kwh
+        self.total_energy_consumed_kwh = total_self_consumption_kwh
+        self.total_energy_fed_in_kwh = total_fed_in_kwh
 
         # Now extract the momentary values
-        str_cur_production = inverter_data["Body"]["Data"]["Site"]["P_PV"]
-        cur_production = float(str_cur_production)
-        str_grid_power = inverter_data["Body"]["Data"]["Site"]["P_Grid"]
-        grid_power = float(str_grid_power)
-        cur_feed_in = grid_power if grid_power > 0.0 else 0.0
-        cur_consumption_from_grid = (-grid_power) if grid_power < 0.0 else 0.0
-        cur_consumption_from_pv = cur_production - cur_feed_in
+        str_cur_production_w = inverter_data["Body"]["Data"]["Site"]["P_PV"]
+        cur_production_kw = float(str_cur_production_w) * 0.001
+        str_grid_power_w = inverter_data["Body"]["Data"]["Site"]["P_Grid"]
+        grid_power_kw = float(str_grid_power_w) * 0.001
+        cur_feed_in_kw = (-grid_power_kw) if grid_power_kw < 0.0 else 0.0
+        cur_consumption_from_grid = grid_power_kw if grid_power_kw > 0.0 else 0.0
+        cur_consumption_from_pv = cur_production_kw - cur_feed_in_kw
         if cur_consumption_from_pv < 0.0:
             cur_consumption_from_pv = 0.0
         cur_consumption_total = cur_consumption_from_grid + cur_consumption_from_pv
@@ -75,15 +75,15 @@ class Fronius:
         # Logging
         if self.verbose_logging:
             print(f"Fronius device: Momentary values:\n"
-                  f" - Current production: {str(cur_production)}\n"
-                  f" - Current feed-in: {str(cur_feed_in)}\n"
+                  f" - Current production: {str(cur_production_kw)} kW\n"
+                  f" - Current feed-in: {str(cur_feed_in_kw)} kW\n"
                   f" - Current consumption from grid: {str(cur_consumption_from_grid)}\n"
                   f" - Current consumption from PV: {str(cur_consumption_from_pv)}\n"
                   f" - Current total consumption: {str(cur_consumption_total)}")
 
         # Store results
-        self.current_power_produced_kw = cur_production
-        self.current_power_fed_in_kw = cur_feed_in
+        self.current_power_produced_kw = cur_production_kw
+        self.current_power_fed_in_kw = cur_feed_in_kw
         self.current_power_consumed_from_grid_kw = cur_consumption_from_grid
         self.current_power_consumed_from_pv_kw = cur_consumption_from_pv
         self.current_power_consumed_total_kw = cur_consumption_total
@@ -92,10 +92,10 @@ class Fronius:
         '''Updates all device stats.'''
         try:
             # Query inverter data
-            r_inverter = requests.get(self.url_inverter, timeout=2)
+            r_inverter = requests.get(self.url_inverter, timeout=5)
             r_inverter.raise_for_status()
             # Query smart meter data
-            r_meter = requests.get(self.url_meter, timeout=2)
+            r_meter = requests.get(self.url_meter, timeout=5)
             r_meter.raise_for_status()
             # Extract and process relevant data
             self.copy_data(r_inverter.json(), r_meter.json())
