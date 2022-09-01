0e5e6ff68d7dc3c17fc8b26a5a7a174a83e5521b
VanKurt <gabrielborisbrock@gmail.com>
2022-09-01 14:53:40 +0200

    Improved test script

--- local_testing/docker-test.sh
+++ local_testing/docker-test.sh
@@ -7,5 +7,5 @@ echo "Running Docker image"
 docker container run -p 8020:5000 -v $(pwd)/data:/data --rm sunalyzer
 
 echo "Cleaning up"
-docker image prune
-docker volume prune
+echo y | docker image prune
+echo y | docker volume prune
