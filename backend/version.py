03ecc3e0ff20e2b2e8a1fa712e6a96e06673909e
Boris Brock <boris@familiebrock.com>
2022-10-04 21:37:23 +0200

    Bumping version to 0.27.2

--- backend/version.py
+++ backend/version.py
@@ -1,4 +1,4 @@
-Version = "0.27.1"
+Version = "0.27.2"
 
 
 def get_version():
--- site/js/version.js
+++ site/js/version.js
@@ -1,5 +1,5 @@
 
-const Version = "0.27.1";
+const Version = "0.27.2";
 
 function setVersion() {
     document.getElementById("version").innerHTML = "Version " + Version;
