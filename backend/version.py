c0cd838ecc6829ba7ea4729b9ed6b23c6ec46950
Boris Brock <boris@familiebrock.com>
2022-10-29 20:11:24 +0200

    Updated version to 0.29.0

--- backend/version.py
+++ backend/version.py
@@ -1,4 +1,4 @@
-Version = "0.28.0"
+Version = "0.29.0"
 
 
 def get_version():
--- site/js/version.js
+++ site/js/version.js
@@ -1,5 +1,5 @@
 
-const Version = "0.28.0";
+const Version = "0.29.0";
 
 function setVersion() {
     document.getElementById("version").innerHTML = "Version " + Version;
